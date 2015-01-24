4e5aa06c39c940053057863a67f5b59642bfdeea
chshouyu <chshouyu@126.com>
2015-01-24 14:30:32 +0800

    add context menu

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,26 @@
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-    return {
-        cancel: true
-    };
-}, {
-    urls: [
-        "http://www.btspread.com/script/jquery.popunder/0.2.27/jquery.popunder.min.js",
-        "http://www.btspread.com/script/jquery.admanager/1.0.0/jquery.admanager.min.js"
-    ]
-}, ["blocking"]);
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+
+    if (info.menuItemId === 'btspread') {
+        var openUrl = 'http://www.btspread.com/search/' + encodeURIComponent(info.selectionText);
+        chrome.tabs.query({
+            url: 'http://www.btspread.com/search/*'
+        }, function(tabs) {
+            if (tabs.length > 0) {
+                chrome.tabs.update(tabs[0].id, {
+                    url: openUrl,
+                    active: true
+                });
+            } else {
+                chrome.tabs.create({
+                    url: openUrl
+                });
+            }
+        });
+    }
+});
+
+chrome.contextMenus.create({
+    "title": "使用btspread搜索 \"%s\"",
+    "contexts": ["selection"],
+    "id": "btspread"
+});
